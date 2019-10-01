e21a29f280cd9ba9a98c68c8b5fb03f9e3e316cb
vincetal <vincent.talaouit@gmail.com>
2019-10-01 15:06:37 +0200

    exo 07 c sans indication précise pour l'utilisateur

---
+++ exo_07_c.rb
@@ -0,0 +1,12 @@
+# demande la saisie une chaine de caractère dans la variable user_name
+user_name = gets.chomp
+
+# écrit la valaue de la variable
+puts user_name
+
+# différence entre 07 et 07_c : le 07 pose une question explicite "puts" avant ce prendre en entrée "gets.chomp"
+# ce qu'il attend
+# le 07_c : ecrit un signe ">" pour guider l'utilisateur dans la saisie de l'entrée pour le "gets.chomp"
+
+# différence entre le 07_b et 07_c : le 07_b guide l'utilisateur dans la saisie
+# alors que le 07_c n'offre aucune indication de cce qu'il attend de l'utilisateur.
