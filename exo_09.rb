b540ca398e15405e8babb921660c4c129056543e
vincetal <vincent.talaouit@gmail.com>
2019-10-01 15:27:18 +0200

    demande nom et prénom et affiche bonjour nom + prenom

---
+++ exo_09.rb
@@ -0,0 +1,6 @@
+puts "Quel est votre nom ? "
+user_name = gets.chomp
+puts "Quel est votre prenom ? "
+user_forename = gets.chomp
+
+puts "Bonjour #{user_forename} #{user_name} , bienvenue à la félicita."
