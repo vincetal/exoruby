6f61281ddedf31cffb7da0b18eadc6b60a4e6ed8
vincetal <vincent.talaouit@gmail.com>
2019-10-01 11:54:19 +0200

    exo 7 b demande de saisir une chaine et stocke en variable sans retour à la ligne

---
+++ exo_07_b.rb
@@ -0,0 +1,11 @@
+# avec le puts , il y a un retour à la ligne
+puts "Bonjour, c'est quoi ton blase ?"
+# avec le print il n'y a pas de retour à la ligne
+# l'affichage se fait directement à la suite
+print "> "
+
+# demande de saisir et stocke dans la variable user_name
+user_name = gets.chomp
+
+# écrit la valeur dela variable
+puts user_name
