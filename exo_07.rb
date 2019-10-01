7070d7ca273982d8f133ccd9f0d28c8532d74975
vincetal <vincent.talaouit@gmail.com>
2019-10-01 11:50:28 +0200

    exo 7 demande de saisir une chaine et stocke en variable

---
+++ exo_07.rb
@@ -0,0 +1,8 @@
+puts "Bonjour, c'est quoi ton blase ?"
+
+# gets.chomp demande de saisir une chaine de caractère
+# et la stocke dans la variable user_name
+user_name = gets.chomp
+
+#écrit le contenu de la variable user_name
+puts user_name
